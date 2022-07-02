cd75ab2fb0f35ad3325b3fbd82bdf0e31529ee34
Lucas L. Batista <lucaslbatista@gmail.com>
2022-07-02 13:16:36 -0300

    feat: add EmployeeController

--- src/main/java/net/javaguides/springboot/controller/EmployeeController.java
+++ src/main/java/net/javaguides/springboot/controller/EmployeeController.java
@@ -2,8 +2,13 @@ package net.javaguides.springboot.controller;
 
 import net.javaguides.springboot.model.Employee;
 import net.javaguides.springboot.service.EmployeeService;
+import org.springframework.beans.BeanUtils;
+import org.springframework.data.repository.query.Param;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/employees")
@@ -15,8 +20,37 @@ public class EmployeeController {
     }
 
     @PostMapping
-    @ResponseStatus(HttpStatus.CREATED)
-    public Employee createEmployee(@RequestBody Employee employee) {
-        return employeeService.saveEmployee(employee);
+    public ResponseEntity<Employee> createEmployee(@RequestBody Employee employee) {
+        return new ResponseEntity(employeeService.saveEmployee(employee), HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<Employee>> getAllEmployees() {
+        return ResponseEntity.ok(employeeService.getAllEmployees());
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Employee> getEmployee(@Param("id") long employeeId) {
+        return employeeService.getEmployeeById(employeeId)
+                .map(ResponseEntity::ok)
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @PutMapping("{id}")
+    public ResponseEntity<Employee> UpdateEmployee(@Param("id") long employeeId,
+                                                   @RequestBody Employee employee) {
+        return employeeService.getEmployeeById(employeeId)
+                .map(savedEmployee -> {
+                    BeanUtils.copyProperties(employee, savedEmployee, "id");
+                    Employee updatedEmployee = employeeService.updateEmployee(savedEmployee);
+                    return ResponseEntity.ok(updatedEmployee);
+                })
+                .orElseGet(() -> ResponseEntity.notFound().build());
+    }
+
+    @DeleteMapping("{id}")
+    public ResponseEntity<String> deleteEmployee(long id) {
+        employeeService.deleteEmployee(id);
+        return new ResponseEntity<>("Employee deleted successfully!.", HttpStatus.NO_CONTENT);
     }
 }
